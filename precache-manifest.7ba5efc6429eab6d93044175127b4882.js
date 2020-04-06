5724de9014d62802cedd484e04eed39d6d185008
Werlesson Vieira <werlessono@gmail.com>
2020-04-06 13:39:31 -0300

    deploy

---
+++ precache-manifest.7ba5efc6429eab6d93044175127b4882.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416cd5399f4e2c852c5d",
+    "url": "/roadmap/css/app.22a57209.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/roadmap/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a8d58550901c12524f97985cef24ba99",
+    "url": "/roadmap/index.html"
+  },
+  {
+    "revision": "6d38febfa16fd1d78d1f",
+    "url": "/roadmap/js/about.ecfce167.js"
+  },
+  {
+    "revision": "416cd5399f4e2c852c5d",
+    "url": "/roadmap/js/app.bee6ad37.js"
+  },
+  {
+    "revision": "184c7e8f5dc01ba51ba3",
+    "url": "/roadmap/js/chunk-vendors.85e6919b.js"
+  },
+  {
+    "revision": "2ee3e2342e2edce7079ab0dbd9321437",
+    "url": "/roadmap/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/roadmap/robots.txt"
+  }
+]);
